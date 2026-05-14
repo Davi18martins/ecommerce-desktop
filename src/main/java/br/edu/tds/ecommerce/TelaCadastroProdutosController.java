08d76e5b067ea2a8f429db8001d946cbab52e1b9
Davi Martins <davi.jesus@alunos.ifsuldeminas.edu.br>
2026-05-14 13:50:26 -0300

    14/05/2026

--- src/main/java/br/edu/tds/ecommerce/TelaCadastroProdutosController.java
+++ src/main/java/br/edu/tds/ecommerce/TelaCadastroProdutosController.java
@@ -6,7 +6,11 @@ package br.edu.tds.ecommerce;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +19,20 @@ import javafx.fxml.Initializable;
  */
 public class TelaCadastroProdutosController implements Initializable {
 
+    @FXML 
+    private TextField txtNome;
+    @FXML 
+    private TextField txtPreco;
+    @FXML 
+    private TextField txtQuantidade;
+    @FXML 
+    private TextField txtImagem;
+    @FXML 
+    private TextArea txtDescricao;
+    @FXML 
+    private ComboBox<String> cbCategoria;
+    
+    
     /**
      * Initializes the controller class.
      */
