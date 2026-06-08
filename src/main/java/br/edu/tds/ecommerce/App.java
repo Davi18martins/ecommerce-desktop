12c1da99fdf9c5bbbcb5d5510a1d3ddeb768a705
Davi Martins <davi.jesus@alunos.ifsuldeminas.edu.br>
2026-06-08 14:19:31 -0300

    08/06/2026

--- src/main/java/br/edu/tds/ecommerce/App.java
+++ src/main/java/br/edu/tds/ecommerce/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("dashboard"), 700, 500);
+        scene = new Scene(loadFXML("dashboard"), 1500, 900);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/br/edu/tds/ecommerce/TelaCadastroProdutosController.java
+++ src/main/java/br/edu/tds/ecommerce/TelaCadastroProdutosController.java
@@ -4,7 +4,11 @@
  */
 package br.edu.tds.ecommerce;
 
+import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,6 +17,9 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -21,110 +28,209 @@ import javafx.scene.control.TextField;
  */
 public class TelaCadastroProdutosController implements Initializable {
 
-    @FXML 
+    @FXML
     private TextField txtNome;
-    @FXML 
+    @FXML
     private TextField txtPreco;
-    @FXML 
+    @FXML
     private TextField txtQuantidade;
-    @FXML 
+    @FXML
     private TextField txtImagem;
-    @FXML 
+    @FXML
     private TextArea txtDescricao;
-    @FXML 
+    @FXML
     private ComboBox<String> cbCategoria;
     @FXML
     private CheckBox cAtivo;
     @FXML
     private Produto produtoEdicao;
-    
-    
+    @FXML
+    private ImageView imgProduto;
+
+    private File arquivoSelecionado;
+    private String caminhoImagem;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        cbCategoria.getItems().add(0,"Nissan");
-        cbCategoria.getItems().add(1,"Mazda");
-        cbCategoria.getItems().add(2,"Toyota");
-        cbCategoria.getItems().add(3,"Honda");
+        cbCategoria.getItems().add(0, "Nissan");
+        cbCategoria.getItems().add(1, "Mazda");
+        cbCategoria.getItems().add(2, "Toyota");
+        cbCategoria.getItems().add(3, "Honda");
     }
 
-    private boolean validarCampos(){
-        
+    private boolean validarCampos() {
+
         boolean validado = true;
-        
-        if(txtNome.getText().isEmpty()){
+
+        if (txtNome.getText().isEmpty()) {
             txtNome.setStyle("-fx-border-color: red; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
             validado = false;
         }
-        if(txtPreco.getText().isEmpty()){
+        if (txtPreco.getText().isEmpty()) {
             txtPreco.setStyle("-fx-border-color: red; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
             validado = false;
         }
-        if(txtQuantidade.getText().isEmpty()){
+        if (txtQuantidade.getText().isEmpty()) {
             txtQuantidade.setStyle("-fx-border-color: red; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
             validado = false;
         }
-        if(txtImagem.getText().isEmpty()){
+        if (txtImagem.getText().isEmpty()) {
             txtImagem.setStyle("-fx-border-color: red; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
             validado = false;
         }
-        
+
         return validado;
-        
-       
+
     }
+
     @FXML
-    private void salvarProduto() throws Exception{
+    private void salvarProduto() throws Exception {
         txtNome.setStyle("-fx-border-color:  black; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
         txtPreco.setStyle("-fx-border-color:  black; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
         txtQuantidade.setStyle("-fx-border-color:  black; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
         txtImagem.setStyle("-fx-border-color:  black; -fx-border-width: 0px0px3px0px; -fx-background-color: transparet;");
-        
-        boolean status= validarCampos();
-        
-        if(status){
+
+        boolean status = validarCampos();
+
+        String caminhoImagem
+                = salvarImagem(
+                        arquivoSelecionado
+                );
+
+        if (status) {
             //salvo os dados do produto no banco de dados
-            
+
             Produto p = new Produto();
             p.setNome(txtNome.getText());
             p.setCategoria(cbCategoria.getValue());
             p.setPreco(Double.parseDouble(txtPreco.getText()));
             p.setQuantidade(Integer.parseInt(txtQuantidade.getText()));
-            p.setImagem(txtImagem.getText());
+            p.setImagem(caminhoImagem);
             p.setDescricao(txtDescricao.getText());
             p.setAtivo(cAtivo.isSelected());
-            
+
             ProdutoDAO dao = new ProdutoDAO();
             dao.cadastrarProduto(p);
             mostrarAlerta("Produto cadastro com sucesso!");
-            
-            
-        }else{
+
+        } else {
             //corrigir informações do formulário
             mostrarAlerta("Todos os campos são obrigatórios");
         }
     }
-    
-    private void mostrarAlerta(String msg){
-    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-    alert.setTitle("Sistemas");
-    alert.setHeaderText(null);
-    alert.setContentText(msg);
-    alert.showAndWait();
+
+    private void mostrarAlerta(String msg) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Sistemas");
+        alert.setHeaderText(null);
+        alert.setContentText(msg);
+        alert.showAndWait();
     }
-    
-    public void setProduto(Produto p){
-        
+
+    public void setProduto(Produto p) {
+
         produtoEdicao = p;
         txtNome.setText(produtoEdicao.getNome());
         cbCategoria.setValue(produtoEdicao.getCategoria());
         txtPreco.setText(String.valueOf(produtoEdicao.getPreco()));
         txtQuantidade.setText(String.valueOf(produtoEdicao.getQuantidade()));
         txtImagem.setText(produtoEdicao.getImagem());
+        Image image
+                = new Image(
+                        new File(
+                                produtoEdicao.getImagem()
+                        )
+                                .toURI()
+                                .toString()
+                );
+
+        imgProduto.setImage(image);
         txtDescricao.setText(produtoEdicao.getDescricao());
         cAtivo.setSelected(produtoEdicao.isAtivo());
     }
+
+    @FXML
+    private void selecionarImagem() {
+
+        FileChooser fileChooser
+                = new FileChooser();
+
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter(
+                        "Imagens",
+                        "*.png",
+                        "*.jpg",
+                        "*.jpeg"
+                )
+        );
+
+        arquivoSelecionado
+                = fileChooser.showOpenDialog(
+                        txtImagem.getScene().getWindow()
+                );
+
+        if (arquivoSelecionado != null) {
+
+            Image image
+                    = new Image(
+                            arquivoSelecionado
+                                    .toURI()
+                                    .toString()
+                    );
+
+            imgProduto.setImage(image);
+
+            txtImagem.setText(arquivoSelecionado.getAbsolutePath());
+        }
+    }
+
+    private String salvarImagem(File arquivo) {
+
+        try {
+
+            String pastaDestino
+                    = "imagens_produtos/";
+
+            File diretorio
+                    = new File(pastaDestino);
+
+            if (!diretorio.exists()) {
+
+                diretorio.mkdir();
+            }
+
+            String nomeArquivo
+                    = System.currentTimeMillis()
+                    + "_"
+                    + arquivo.getName();
+
+            Path origem
+                    = arquivo.toPath();
+
+            Path destino
+                    = Path.of(
+                            pastaDestino,
+                            nomeArquivo
+                    );
+
+            Files.copy(
+                    origem,
+                    destino,
+                    StandardCopyOption.REPLACE_EXISTING
+            );
+
+            return destino.toString();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
 }
